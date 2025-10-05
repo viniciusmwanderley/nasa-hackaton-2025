4b2fdea1d2c4d0b798ba5a7fc3bb767e3160a362
Luana de Méro <luanamero4@gmail.com>
2025-10-05 11:27:33 -0300

    upgrading dashboard

--- front/src/components/layout/ActionBar/ActionBar.tsx
+++ front/src/components/layout/ActionBar/ActionBar.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import './ActionBar.css';
+import { utils, writeFile } from 'xlsx';
 
 interface ActionBarProps {
     onGenerateReport: () => void;
-    onExport: () => void;
 }
 
-const ActionBar: React.FC<ActionBarProps> = ({ onGenerateReport, onExport }) => {
+const ActionBar: React.FC<ActionBarProps> = ({ onGenerateReport }) => {
+    const handleCreateReport = () => {
+        // Placeholder data for the CSV
+        const data = [
+            { Column1: 'Value1', Column2: 'Value2', Column3: 'Value3' },
+            { Column1: 'Value4', Column2: 'Value5', Column3: 'Value6' },
+        ];
+
+        // Convert data to worksheet
+        const worksheet = utils.json_to_sheet(data);
+        const workbook = utils.book_new();
+        utils.book_append_sheet(workbook, worksheet, 'Report');
+
+        // Write workbook and trigger download
+        writeFile(workbook, 'report.xlsx');
+    };
+
     return (
         <div className="action-bar-container">
-            <button className="report-button" onClick={onGenerateReport}>
-                Gerar Relatório
-            </button>
-            <button className="export-button" onClick={onExport}>
-                Exportar
+            <button className="report-button" onClick={handleCreateReport}>
+                Create Report
             </button>
         </div>
     );
